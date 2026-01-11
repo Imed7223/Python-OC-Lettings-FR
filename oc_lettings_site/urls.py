7dbb8aa7367b4101005b91474f95d1516611f408
IMED-EDDINE MIHOUBI <mihoubiimededdine@gmail.com>
2026-01-11 23:35:57 +0100

    Refactor project into modular apps (oc_lettings_site, lettings, profiles)

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-
-from . import views
+from django.urls import path, include
+from oc_lettings_site import views as site_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('', site_views.index, name='index'),
+    path('lettings/', include('lettings.urls')),
+    path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
