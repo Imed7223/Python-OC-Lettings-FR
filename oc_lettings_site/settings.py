6c921e3aec58aa199e92e805e6e473976563af7a
Imed7223 <mihoubiimededdine@gmail.com>
2026-03-30 19:07:03 +0200

    admin2

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -159,7 +159,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configuration pour servir les fichiers statiques en production avec WhiteNoise
