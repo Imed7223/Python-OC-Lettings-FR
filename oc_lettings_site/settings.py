b33dfa9dafc5ea88c5d0236874e5262eb40e3dce
Imed7223 <mihoubiimededdine@gmail.com>
2026-04-01 00:28:38 +0200

    settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -187,3 +187,6 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "propaie19@gmail.com")
 CONTACT_EMAIL = os.getenv("CONTACT_EMAIL", "propaie19@gmail.com")
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
