242b3dc6dfb7cabf2811a7d6b5edc67614a2284c
Imed7223 <mihoubiimededdine@gmail.com>
2026-03-31 21:55:32 +0200

    background

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -145,8 +145,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
